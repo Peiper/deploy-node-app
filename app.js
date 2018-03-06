0d45f73c6d7e27eecdb8d4e531c4e3c995c6509a
peiper <niklas.peiper@gmail.com>
2018-03-06 09:58:30 +0100

    enable cors

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var exec = require('child_process').execSync;
 var cron = require('node-cron');
-
+var cors = require('cors')
 var ravenHelper = require('./ravenHelper');
 
 var app = express();
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
